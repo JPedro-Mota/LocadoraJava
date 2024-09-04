f3b2c013ca045d2e639c231ccfe5acafe08e2469
João Pedro Mota <joaopedromota67@gmail.com>
2024-09-03 21:42:30 -0300

    ajustment in CORS

--- locadora/src/main/java/com/springboot/locadora/configurations/SecurityConfig.java
+++ locadora/src/main/java/com/springboot/locadora/configurations/SecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -31,9 +33,10 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration corsConfig = new CorsConfiguration();
-                    corsConfig.applyPermitDefaultValues();
-                    corsConfig.addAllowedOrigin("");
-                    corsConfig.addAllowedMethod("");
+                    corsConfig.setAllowedOrigins(List.of("http://localhost:9000"));  // Especifique a origem permitida
+                    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // Métodos permitidos
+                    corsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type"));  // Cabeçalhos permitidos
+                    corsConfig.setAllowCredentials(true);  // Permitir o envio de credenciais
                     return corsConfig;}))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
